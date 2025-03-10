f524f9bf8a99f2d384ef29d4ea493aaa8456ea77
Aun Mohammad <aun0545088@gmail.com>
2025-03-10 17:39:24 +0530

    test the json

---
+++ index.js
@@ -0,0 +1,14 @@
+const data = `{
+    "name":"Aun",
+    "city":"kanpur"
+}`
+
+const nData = {
+    name:"Aun Mohammad",
+    city:"Delhi"
+}
+
+console.log(JSON.parse(data))
+
+console.log(JSON.stringify(nData))
+
